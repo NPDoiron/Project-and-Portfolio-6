1fe2c4ef3fcc49249e889977168b89b822f31f7b
Nicholas Doiron <58046157+NPDoiron@users.noreply.github.com>
2020-03-12 18:45:25 -0500

    Initial Creation
    Created project

---
+++ DungeonsandDragonsTraven/app/src/main/java/com/example/dungeonsanddragonstraven/menu_screens/MainMenu.java
@@ -0,0 +1,28 @@
+package com.example.dungeonsanddragonstraven.menu_screens;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainMenu extends Fragment {
+
+    public static MainMenu newInstance() {
+
+        Bundle args = new Bundle();
+
+        MainMenu fragment = new MainMenu();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        re
+    }
+}
